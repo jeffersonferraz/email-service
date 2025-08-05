f648b5a9751b914528fc204e6d69b81ee0438402
Jefferson Ferraz <jefferson.ferraz@live.com>
2025-08-05 15:54:53 +0200

    Update email subject

--- api/send-email.js
+++ api/send-email.js
@@ -20,7 +20,7 @@ module.exports = async (req, res) => {
   const mailOptions = {
     from: email,
     to: process.env.EMAIL_USER,
-    subject: `New message from ${name}`,
+    subject: `Portfolio: contact form message (${name})`,
     text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
   };
 
