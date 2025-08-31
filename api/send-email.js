8e1a5b5bafbc93fafecf9d0f4c1936818fd3b2a5
Jefferson Ferraz <jefferson.ferraz@live.com>
2025-08-31 16:02:06 +0200

    added boolean variable submitted

--- api/send-email.js
+++ api/send-email.js
@@ -27,7 +27,7 @@ module.exports = async (req, res) => {
   try {
     await transporter.sendMail(mailOptions);
     //res.status(200).send("Email sent successfully!");
-    res.redirect(301, "https://jeffersonferraz.github.io");
+    res.redirect(301, "https://jeffersonferraz.github.io/?submitted=true");
   } catch (error) {
     console.error(error);
     res.status(500).send("Failed to send email.");
